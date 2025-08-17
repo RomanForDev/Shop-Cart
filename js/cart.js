cf2c0d637d16b1db48fefa76c71ba141125956b2
RomanForDev <romanfortinidev@gmail.com>
2025-08-17 17:31:18 -0300

    agregar al carrito listo

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,22 @@
 function addToCart(producto) {
-    const memoria = localStorage.getItem("products");
-    if(!memoria) {
-        const nuevoProducto = producto;
-        nuevoProducto.cantidad = 1;
+    const memory = JSON.parse(localStorage.getItem("products"));
+    if(!memory) {
+        const nuevoProducto = addToMemory(producto);
         localStorage.setItem("products", JSON.stringify([nuevoProducto]));
     } else {
-        if(nuevoProducto)
+        const indiceProducto = memory.findIndex(shoe => shoe.id === producto.id);
+        const newMemory = memory;
+        if(indiceProducto === -1) {
+            newMemory.push(addToMemory(producto));
+        } else {
+            newMemory[indiceProducto].cantidad ++;
+        }
+        localStorage.setItem("products", JSON.stringify(newMemory));
     }
+}
+
+function addToMemory(producto) {
+    const newProduct = producto;
+    newProduct.cantidad = 1
+    return newProduct;
 }
--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ function setCards(productos) {
         card.innerHTML = `<img class= "shoe-img" src="${shoe.img}">
         <h3 id="shoe-name" class="shoe-name" >${shoe.name}</h3>
         <p id="shoe-price" class= "shoe-price">${shoe.price}</p>
+        <p id="shoe-id" class= "shoe-id">${shoe.id}</p>
         <button id="add-button" class="cart-button">Agregar</button>`
         nuevaCard.appendChild(card);
         card.getElementsByClassName("cart-button")[0].addEventListener("click", ()=> addToCart(shoe));
