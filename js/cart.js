2e9b746e7422c4a18d25854c92a4da48101ed58a
RomanForDev <romanfortinidev@gmail.com>
2025-08-18 17:47:36 -0300

    logica para mostrar el número de items del carrito casi terminada. Falta hacer que aparezca desde el item numero 1

--- js/cart.js
+++ js/cart.js
@@ -12,6 +12,9 @@ function addToCart(producto) {
             newMemory[indiceProducto].cantidad ++;
         }
         localStorage.setItem("products", JSON.stringify(newMemory));
+        sumNumber(newMemory);
+        // console.log(newMemory);
+        
     }
 }
 
@@ -20,3 +23,14 @@ function addToMemory(producto) {
     newProduct.cantidad = 1
     return newProduct;
 }
+
+let numberCart = document.getElementById("items-cart");
+
+function sumNumber(array){
+    let cantidades = array.map(objeto => objeto.cantidad);
+    let sum = cantidades.reduce((a, b) => a + b, 0);
+        console.log(sum);
+    }
+
+
+//Suma las cantidades sólo si son items distintos. Cuando se suma un item igual no suma ya que no es un nuevo elemento dentro del array sino que cambia el valor de una de sus claves. Yo tendría que acceder al valor de la clave cantidad en cada objeto para asignarlo al length.
