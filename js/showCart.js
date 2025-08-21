0789530687010c022973db1bf1da53c584c71a35
RomanForDev <romanfortinidev@gmail.com>
2025-08-21 19:08:12 -0300

    correcciones

--- js/showCart.js
+++ js/showCart.js
@@ -1,20 +1,42 @@
 // Funcion para escribir el HTML con la información del carrito.
 // const cart = [];
-const cartView = document.getElementById('cart-view');
+// const cartView = document.getElementById('cart-view');
 
-const memory = JSON.parse(localStorage.getItem("products"));
+// const memory = JSON.parse(localStorage.getItem("products"));
 
-function writeCart(arr) {
-    if(!arr) {
-        console.log("No hay productos en el carrito");
-    }else {
-        arr.forEach(item => {
-            const itemView = document.createElement("div");
-            itemView.classList = "item";
-            itemView.innerHTML = `<p>${item}</p>`;
-            cartView.appendChild(itemView);
-            // console.log(memory)
-    })}
-}
+// function writeCart(arr) {
+//     if(!arr) {
+//         console.log("No hay productos en el carrito");
+//     }else {
+//         arr.forEach(item => {
+//             const itemView = document.createElement("div");
+//             itemView.classList = "item";
+//             itemView.innerHTML = `<p>${item}</p>`;
+//             cartView.appendChild(itemView);
+//             // console.log(memory)
+//     })}
+// }
 
-writeCart(memory);
+// writeCart(memory);
+
+const nuevaCard = document.getElementById("cards-cart");
+
+function showCards() {
+    const products = JSON.parse(localStorage.getItem("products"));
+    if(products && products.length > 0){
+    products.forEach(producto => {
+        const card = document.createElement("div");
+        card.classList = "shoe-card";
+        card.innerHTML = `
+        <img class= "shoe-img" src="${producto.img}">
+        <h3 id="shoe-name" class="shoe-name" >${producto.name}</h3>
+        <p id="shoe-price" class= "shoe-price">${producto.price}</p>
+        <p id="shoe-id" class= "shoe-id">${producto.id}</p>
+        <button id="add-button" class="cart-button">Agregar</button>
+        `
+        nuevaCard.appendChild(card);
+        card.getElementsByClassName("cart-button")[0].addEventListener("click", ()=> addToCart(shoe));
+    });
+}}
+
+showCards();
