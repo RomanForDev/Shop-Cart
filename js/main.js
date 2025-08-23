8c4074773f9197b1cf41a34b45713b76cadad2f6
RomanForDev <romanfortinidev@gmail.com>
2025-08-23 17:53:45 -0300

    estilos para cards de index y para el carrito de compras. Aún no terminado. Resta corregir el grid para que haga autofill así no rompe las mediaQ

--- js/main.js
+++ js/main.js
@@ -9,8 +9,7 @@ function setCards(productos) {
         card.classList = "shoe-card";
         card.innerHTML = `<img class= "shoe-img" src="${shoe.img}">
         <h3 id="shoe-name" class="shoe-name" >${shoe.name}</h3>
-        <p id="shoe-price" class= "shoe-price">${shoe.price}</p>
-        <p id="shoe-id" class= "shoe-id">${shoe.id}</p>
+        <p id="shoe-price" class= "shoe-price">$${shoe.price}</p>
         <button id="add-button" class="cart-button">Agregar</button>`
         nuevaCard.appendChild(card);
         card.getElementsByClassName("cart-button")[0].addEventListener("click", ()=> addToCart(shoe));
